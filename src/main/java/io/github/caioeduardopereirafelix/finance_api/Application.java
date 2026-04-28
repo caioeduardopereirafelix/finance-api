b28812cfe365283484bafd17f2e3a6a4e5877c82
Caio Eduardo <caiotech02@gmail.com>
2026-04-28 16:20:33 -0300

    feature: create externalized configuration db

--- src/main/java/io/github/caioeduardopereirafelix/finance_api/Application.java
+++ src/main/java/io/github/caioeduardopereirafelix/finance_api/Application.java
@@ -1,6 +1,6 @@
 package io.github.caioeduardopereirafelix.finance_api;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
